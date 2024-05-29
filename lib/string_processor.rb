3ac0056b0261d9f48cbf75f0a38ea3ea766bc0f9
Ankit <ankitrathoretech@gmail.com>
2024-05-29 14:59:33 +0530

    Implemented support for custom delimiters of any length and handling multiple custom delimiters

--- lib/string_processor.rb
+++ lib/string_processor.rb
@@ -3,7 +3,10 @@ class StringProcessor
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    nums = numbers.split(",").map(&:to_i)
+    delimiter, numbers = extract_delimiter_and_numbers(numbers)
+
+    # Split the numbers using the delimiter, convert them to integers
+    nums = numbers.split(delimiter).map(&:to_i)
 
     check_for_negatives(nums)
 
@@ -12,6 +15,33 @@ class StringProcessor
   end
 
   private
+
+  # Method to extract delimiter and numbers from the input string
+  def self.extract_delimiter_and_numbers(numbers)
+
+    if numbers.start_with?('//')
+      header, numbers = numbers.split("\n", 2)
+
+      delimiters = extract_custom_delimiters(header)
+      # Return the delimiter and numbers
+      [Regexp.union(delimiters), numbers]
+    else
+      # If no custom delimiter is specified, use default delimiters (comma or newline)
+      [/[,|\n]/, numbers]
+    end
+  end
+
+  # Method to extract custom delimiters from the header
+  def self.extract_custom_delimiters(header)
+
+    if header.include?('[')
+      # Extract custom delimiters enclosed in square brackets
+      header.scan(/\[(.*?)\]/).flatten
+    else
+      # Extract single custom delimiter
+      [header[2..-1]]
+    end
+  end
 
   # Method to check for negative numbers and raise an exception if any are found
   def self.check_for_negatives(nums)
