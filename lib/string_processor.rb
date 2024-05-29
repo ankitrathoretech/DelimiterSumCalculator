7d18d3114d4a3b20dbad3342c23d1431ef9f6002
Ankit <ankitrathoretech@gmail.com>
2024-05-29 14:56:48 +0530

    Added functionality to raise an exception for negative numbers

--- lib/string_processor.rb
+++ lib/string_processor.rb
@@ -3,6 +3,20 @@ class StringProcessor
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(",").map(&:to_i).sum
+    nums = numbers.split(",").map(&:to_i)
+
+    check_for_negatives(nums)
+
+    # Return the sum of the numbers
+    nums.sum
+  end
+
+  private
+
+  # Method to check for negative numbers and raise an exception if any are found
+  def self.check_for_negatives(nums)
+    negatives = nums.select { |num| num < 0 }
+
+    raise "negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
   end
 end
